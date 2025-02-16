f93c17aa278062b20c7ce49d483418c1b4ac5c9e
Szymon Kowaliński <szymon@kowalinski.dev>
2025-02-16 16:35:47 +0100

    init: add serverpod admin template

---
+++ serverpod_admin_panel/.adminjs/.entry.js
@@ -0,0 +1,7 @@
+AdminJS.UserComponents = {}
+import Dashboard from '../components/dashboard'
+AdminJS.UserComponents.Dashboard = Dashboard
+import Custom from '../components/custom'
+AdminJS.UserComponents.Custom = Custom
+import PostgresQuery from '../components/postgres_query'
+AdminJS.UserComponents.PostgresQuery = PostgresQuery
